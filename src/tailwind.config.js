005c39b11a0d2a8e7d45f5758dcf8d2af61b3dc4
kazuki1023 <k.keio12569@gmail.com>
2023-12-08 00:42:17 +0900

    prettierrcで整形

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -7,6 +7,8 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        '/resources/views/components/**/*.blade.php',
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
@@ -14,8 +16,13 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            spacing: {
+                34: '8.5rem', // 136px
+                34.5: '8.625rem', // 138px
+                35: '8.75rem', // 140px
+            },
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, require('flowbite/plugin')],
 };
