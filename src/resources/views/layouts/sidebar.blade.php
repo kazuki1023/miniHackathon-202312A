d67c8f805a453a30c46e8833d054a0331fd32ba2
kazuki1023 <k.keio12569@gmail.com>
2023-12-08 01:04:07 +0900

    feat: :art: topに固定した

--- src/resources/views/layouts/sidebar.blade.php
+++ src/resources/views/layouts/sidebar.blade.php
@@ -13,7 +13,7 @@
 <aside id="separator-sidebar"
     class="fixed top-34.5 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
     aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto bg-white dark:bg-gray-800">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-100 dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
             <li>
                 <a href="#"
