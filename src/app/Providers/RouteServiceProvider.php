9cfb6492c2c27f870a43fda0de745420b274fdc3
Goki Kanno <goki.kanno@gmail.com>
2023-12-09 20:19:26 +0900

    ログインしたら管理者一覧に飛ぶよ

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -11,15 +11,6 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * The path to your application's "home" route.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/dashboard';
-
-    /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
     public function boot(): void
@@ -36,5 +27,21 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+    }
+
+    /**
+     * Get the path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @return string
+     */
+    public static function getHome(): string
+    {
+        // Assuming Auth::user() returns the authenticated user
+        // You may need to adjust this part based on your authentication logic
+        $clubId = optional(auth()->user())->club_id;
+
+        return '/club/index/' . $clubId;
     }
 }
