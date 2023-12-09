c518eec9d5b75a8215ec593da63509509eee9f75
dukken06 <kenxavi.8@gmail.com>
2023-12-09 13:41:23 +0900

    rolesテーブル作成

--- src/database/migrations/2023_12_09_042403_create_create_roles_tables_table.php
+++ src/database/migrations/2023_12_09_042403_create_create_roles_tables_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('create_roles_tables', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name')->comment('権限名');
         });
     }
 
