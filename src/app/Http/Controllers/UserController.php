e9e1ca43bb6abfa7bf931d1c712f13357ba4bd6f
uenoarisa <arisa-choco0326@docomo.ne.jp>
2023-12-09 17:46:09 +0900

    ♻️ servicesフォルダにUserServiceを切り出し

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -5,22 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Services\UserService;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(UserService $userService)
     {
         $user = Auth::user();
         $role = $user->role;
 
-        if ($role == 1) {
-            // 管理者1の場合、同じサークルの管理者のみを取得
-            $managers = User::where('club_id', $user->club_id)->get();
-        } elseif ($role == 2) {
-            // 管理者2の場合、全ての管理者を取得
-            $managers = User::with('club')->get();
-            
-        }
+        $managers = $userService->getManagers($user);
         return view('dashboard', compact('managers', 'role'));
     }
 
